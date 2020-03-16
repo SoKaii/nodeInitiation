fb2713a770039c09bc13d2a89acd780205ac628b
SoKaii <t.martin92500@hotmail.Fr>
2020-03-16 15:58:37 +0100

    Created a post route to answer to the user

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.get('/',function (req,res) {
     res.send("Hello World !")
 })
@@ -11,6 +13,20 @@ app.get('/hello',function (req,res) {
         res.send("Bonjour, " + req.query.nom)
     } else {
         res.send("Quel est votre nom ? ")
+    }
+})
+
+app.post('/chat',function (req, res) {
+    switch(req.body.msg) {
+        case "ville" : 
+            res.send("Nous sommes à Paris\n");
+            break;
+        case "meteo" : 
+            res.send("Il fait beau\n");
+            break;
+        default : 
+            res.send("Réponse par défaut\n")
+            break;
     }
 })
 
