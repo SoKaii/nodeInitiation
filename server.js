26d271a3caad287d185e00fce9767cdcd51161d8
SoKaii <t.martin92500@hotmail.Fr>
2020-03-16 15:03:07 +0100

    added the id to personnalize the message

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/',function (req,res) {
     res.send("Hello World !")
 })
 
+app.get('/hello/:id',function (req,res) {
+    res.send("Bonjour, " + req.params.id)
+})
+
 app.listen(PORT,function () {
     console.log('Example app listening on port ${PORT}!')
 })
