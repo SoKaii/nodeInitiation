f92c09b87142ac7bee7843f96ce36804db9ca513
SoKaii <t.martin92500@hotmail.Fr>
2020-03-18 14:25:02 +0100

    Link the server.js to the database , and added a DELETE endpoint

--- server.js
+++ server.js
@@ -1,55 +1,124 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'chat-bot';
 const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const fs = require('fs')
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(express.json());
+const fs = require('fs');
 
-app.use(express.json())
-
-app.get('/',function (req,res) {
-    res.send("Hello World !")
+app.get('/', function (req, res) {
+      res.send('Hello World');
 })
 
-app.get('/hello',function (req,res) {
-    if(req.query.nom) {
-        res.send("Bonjour, " + req.query.nom)
-    } else {
-        res.send("Quel est votre nom ? ")
+app.get('/hello', function (req, res) {
+    if (req.query.nom) {
+      res.send('Bonjour, '+req.body.nom+' !');
+    }else {
+      res.send('Quel est votre nom ?');
     }
 })
 
-app.post('/chat', (req, res) => {
-    var message = req.body.msg
-    switch(message) {
-      case "ville":
-        res.send("Nous sommes à Paris\n");
-        break;
-      case "météo":
-        res.send("Il fait beau\n");
-        break;
-      case "demain":
-        let rawdata = fs.readFileSync('reponses.json');
-        let json = JSON.parse(rawdata);
-        console.log(json.day);
-        if(json.day == null) {
-          res.send("Je ne connais pas demain…\n");
+app.post('/chat', function (req, res) {
+  (async function() {
+    const client = new MongoClient(url);
+    try {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db(dbName);
+      const col = db.collection('messages');
+      if (req.body.msg === 'ville') {
+        let r = await db.collection('messages').insertMany([{from : "user",message : req.body.msg},{from : "bot",message : 'Nous sommes à Paris'}]);
+        assert.equal(2, r.insertedCount);
+        res.send('Nous sommes à Paris')
+      } else if (req.body.msg === 'météo') {
+        let r = await db.collection('messages').insertMany([{from : "user",message : req.body.msg},{from : "bot",message : 'Il fait beau'}]);
+        assert.equal(2, r.insertedCount);
+        res.send('Il fait beau')
+      } else {
+        if (/ = /.test(req.body.msg)) {
+          const [ cle, valeur ] = req.body.msg.split(' = ')
+          const valeursExistantes = readValuesFromFile();
+          fs.writeFileSync('response.json', JSON.stringify({
+            ...valeursExistantes,
+            [cle]: valeur
+          }))
+          res.send('Merci pour cette information !')
         } else {
-          res.send(json.day);
+          const cle = req.body.msg
+          const reponse = readValuesFromFile()[cle]
+          res.send(cle + ': ' + reponse)
         }
-        break;
-      case (message.match(/^demain = /) || {}).input:
-        let day = {
-            day: message.substring(9),
-        };
-        let data = JSON.stringify(day);
-        fs.writeFileSync('reponses.json', data);
-        res.send("Merci pour cette information !");
-        break;
-      default:
-        res.send("Réponse par défaut\n");
-        break;
+      }
+      const cursor = col.find();
+      while(await cursor.hasNext()) {
+      doc = await cursor.next();
+      console.dir(doc);
+  }
+  res.send(doc)
+    } catch (err) {
+      console.log(err.stack);
     }
-  });
+    // Close connection
+    client.close();
+  })();
 
-app.listen(PORT,function () {
-    console.log('Example app listening on port ${PORT}!')
+})
+
+function readValuesFromFile() {
+  const reponses = fs.readFileSync('response.json', { encoding: 'utf8' });
+  const valeursExistantes = JSON.parse(reponses);
+  return valeursExistantes;
+}
+
+app.get('/messages/all',function(req,res){
+var doc = [];
+  (async function() {
+    const client = new MongoClient(url);
+    try {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db(dbName);
+      const col = db.collection('messages');
+      const cursor = col.find();
+      // Iterate over the cursor
+      var i=1
+      while(await cursor.hasNext()) {
+      doc[i] = await cursor.next();
+
+      i=i+1;
+  }
+   console.log(doc);
+   res.send(doc)
+    } catch (err) {
+      console.log(err.stack);
+    }
+    // Close connection
+    console.log("Client close");
+    client.close();
+  })();
+})
+app.delete('/messages/last',function(req,res){
+  (async function() {
+    const client = new MongoClient(url);
+    try {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db(dbName);
+      const col = db.collection('messages');
+      for(i=0;i<2;i++) {
+          col.findOneAndDelete({},{"sort": { "_id": -1 }})
+        }
+res.send('last conversation deleted');
+  }catch (err) {
+    console.log(err.stack);
+  }
+client.close();
+})();
+})
+
+app.listen(process.env.PORT || 3000 , function () {
+  console.log('Example app listening on port 3000!')
 })
